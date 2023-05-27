c29ea52c27da657b7108bd98fd6b979c91fb7e7b
Fayyaz Ali <faiz48097@gmail.com>
2023-05-27 14:15:40 +0500

    Html and CSS test of apartment rent templete

---
+++ resources/js/script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+  //   const romoveIcon = document.querySelector("i#icon");
+  //   const model1 = document.querySelector("#subcriber");
+  //   romoveIcon.addEventListener("click", (event) => {
+  //     romoveIcon.classlist.remove("model");
+  //   });
+
+  function mYFUNCTION() {
+    document.getElementById("subcribe").style.display = "none";
+  }
+};
